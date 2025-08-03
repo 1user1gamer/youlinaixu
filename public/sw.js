25fe8ec3307b610221ca716d6babbb56ba8ad9b9
liumou <email@example.com>
2025-08-04 00:05:33 +0800

    强制覆盖前的提交

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1754233194875";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1754236413060";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
